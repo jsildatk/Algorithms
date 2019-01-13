f27423ad46c9f2f19eafdd16dc8408444fff8a3e
jsildatk <jakub.sildatk@gmail.com>
2019-01-13 15:54:48 +0100

    Fix

--- hashTables/6.3/Hash.java
+++ hashTables/6.3/Hash.java
@@ -4,41 +4,26 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Hash {
-	public static int hash(String text, int m) {
+	private static int hashString(String text) {
 		int hash = 0;
-		int length = text.length();
-		int letter;
-		int code;
-        for (int i = 0; i < length; i++){
-            letter = text.charAt(i);
-            code = (1 + (letter % (m-2))) * i;
-            hash += letter + code;
-        }
-        return hash % m;
+		for (int i = 0; i < text.length(); i++) {
+			hash += text.charAt(i);
+		}
+		return hash;
+	}
+
+	private static int hash(int k, int m, int i) {
+		int mod1 = k % m;
+		int mod2 = i * (1 + k % (m - 2));
+		return (mod1 + mod2) % m;
 	}
 	
 	public static void hashValues(ArrayList<Record> data, ArrayList<Record> result, int m) {
-		int index;
-		int start;
-		for (int i = 0; i < data.size(); i++) {
-			index = hash(data.get(i).getSurname(), m);
-			start = index;
-			while (index < data.size() && result.get(index).getNumber() != "0" ) {
-				index++;
-			}
-			if (index < data.size() && result.get(index).getNumber() == "0") {
+		for (int i = 0; i < m; i++) {
+			int index = hash(hashString(data.get(i).getSurname()), m, i);
+			if (result.get(index).getNumber() == "0") {
 				result.get(index).setNumber(data.get(i).getNumber());
 				result.get(index).setSurname(data.get(i).getSurname());
-			}
-			if (index == data.size()) {
-				index = 0;
-				while (index < start && result.get(index).getNumber() != "0") {
-					index++;
-				}
-				if (index < start && result.get(index).getNumber() == "0") {
-					result.get(index).setNumber(data.get(i).getNumber());
-					result.get(index).setSurname(data.get(i).getSurname());
-				}
 			}
 		}
 	}
